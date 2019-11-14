70e4df00c6d23249cb9c55fb2674f465b27f766c
Benjamin-DCI <47890043+Benjamin-DCI@users.noreply.github.com>
2019-11-14 10:51:54 +0100

    moodelss

---
+++ models/Order.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+  quantity: {
+    type: Number,
+    required: true
+  },
+  record: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
---
+++ models/Record.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const RecordSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+
+  artist: {
+    type: String,
+    required: true
+  },
+
+  year: {
+    type: Number,
+    required: true
+  },
+  img: {
+    type: String,
+    required: true
+  },
+
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Record", RecordSchema);
